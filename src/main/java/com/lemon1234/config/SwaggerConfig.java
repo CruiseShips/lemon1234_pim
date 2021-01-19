c87deae0652c98a38ad9b19e2c89f262df51f0d8
chenzhihua <chenzhihua@tobosoft.com.cn>
2021-01-19 10:53:49 +0800

    lemon1234_pim

---
+++ src/main/java/com/lemon1234/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.lemon1234.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2 配置类
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket createRestApi() {
+		
+		StringBuffer sBuffer = new StringBuffer();
+		sBuffer.append("每一个星球都有一个驱动核心， \n");
+		sBuffer.append("每一种思想都有影响力的种子。\n");
+		sBuffer.append("感受世界的温度， \n");
+		sBuffer.append("年轻的你也能成为改变世界的动力!");
+		
+		// 配置一下 Swagger 文档类型
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
+		docket.pathMapping("/")
+			  .select()
+			  // 配置我们扫描的包 也可以配置成其他类型
+			  .apis(RequestHandlerSelectors.basePackage("com.lemon1234.controller"))
+			  .paths(PathSelectors.any())
+			  // 添加一些 ApiInfo
+			  .build().apiInfo(new ApiInfoBuilder()
+                      .title("PIM 接口文档")
+                      .description(sBuffer.toString())
+                      .version("1.0.0")
+                      .license("Apache 2.0 License")
+                      .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                      .build());
+		return docket;
+	}
+}
